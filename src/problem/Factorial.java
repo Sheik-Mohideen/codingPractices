94683dbeafeb36fde409b9a52fe38612e0784151
Sheik Mohideen <51860338+Sheik-Mohideen@users.noreply.github.com>
2020-12-22 14:42:10 +0530

    Revert "Algorithm"
    This reverts commit f9d287aff430905d8706fa5c8e43d195c778f353.

---
+++ src/problem/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int a,i;double sum=1;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the factorial value");
+        a=s.nextInt();
+        for (i=1;i<=a;i++)
+        {
+            sum=sum*i;
+        }
+        System.out.println("The factorial of "+a+"is "+sum);
+    }
+}
---
+++ src/problem/Insertionsort.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Insertionsort {
+    public static void main(String[] args) {
+        int i, n, j, r,t;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Number of Elements");
+        n = s.nextInt();
+        int[] a = new int[n];
+        for (i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+                for(i=0;i<n;i++)
+                {
+                    if(a[i]>a[i+1])
+                    {
+                        t=a[i];
+                        a[i]=a[i+1];
+                        a[i+1]=t;
+
+                    }
+                    for(j=0;j<i+1;j++)
+                    {
+                        if(a[j]>a[j+1])
+                        {
+                            r=a[j];
+                            a[j]=a[j+1];
+                            a[j+1]=r;
+
+                        }
+                    }
+                }
+                for(i=0;i<n;i++)
+                {
+                    System.out.println(a[i]);
+                }
+
+    }
+}
